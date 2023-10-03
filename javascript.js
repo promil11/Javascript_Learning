dfe2c5ac40a27163544cbd655178a416a599bc24
promil11 <promil.j@cisinlabs.com>
2023-10-03 20:43:24 +0530

    dev:promise,async,await

--- javascript.js
+++ javascript.js
@@ -1,117 +1,82 @@
 
-// Call, Apply, Bind, Map, Filter, Reduce, Splice, Slice
+// Promise --->are solution of callback hell
+// three state-->1)pending  2)resolve  3)reject 
 
-    // //call
-    // const person1 = {
-    //     fName : "promil",
-    //     lName : "jain",
-    //     showFullName : function(company, hometown){
-    //         console.log(this.fName + " " + this.lName + company + hometown );
-    //     }
-    // }
+// console.log("START");
 
-    // const person2 = {
-    //     fName : "tanu",
-    //     lName : "jain",
-    // }
+// const promiseObject = new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         resolve("data fetch successfully....")
+//         // reject("api doesn't work");
+//     },3000);
+//     // reject("api doesn't work");
+// }) 
 
-    // console.log(person1.showFullName()); //promil jain
-    
-    // console.log(person1.showFullName.call(person2, "amazon" , "indore")); //tanu jain
+// const promiseObject2 = new Promise(function(resolve,reject){
+//     setTimeout(()=>{
+//         resolve("data2 fetch successfully....")
+//     },1000);
+//     // reject("api doesn't work");
+// }) 
 
-
-
-     //apply
-//    const person1 = {
-//         fName : "promil",
-//         lName : "jain",
-//         showFullName : function(company, hometown){
-//             console.log(this.fName + " " + this.lName + " "+ company +" " + hometown );
-//         }
-//     }
-
-//     const person2 = {
-//         fName : "tanu",
-//         lName : "jain",
-//     }
-
-
-//     console.log(person1.showFullName.apply(person2, ["amazon" , "indore"])); //tanu jain amazon indore
-
-
-
-//bind
-//    const person1 = {
-//         fName : "promil",
-//         lName : "jain",
-//         showFullName : function(company, hometown){
-//             console.log(this.fName + " " + this.lName + " "+ company +" " + hometown );
-//         }
-//     }
-
-//     const person2 = {
-//         fName : "tanu",
-//         lName : "jain",
-//     }
-
-    
-//     const result = person1.showFullName.bind(person2, ["amazon" , "indore"]);
-//     // console.log(result); //return full copy of function 
-//     result();
-
-
-// Map , Filter, Reduce are array method
-//map 
-// 1) Not manipulate the original array
-// 2) create new array and copy all manipulate element into it
-    // const array1 = [2,3,1,4,8,7,9];
-    // const newArray = array1.map((element)=>{
-    //     return element*2;
-    // })
-    // console.log(newArray);
-    // console.log(array1);
-
-
-//filter
-// 1) Not manipulate the original array
-// 2) create new array and copy all manipulate element into it
-// const array2 = [3,2,5,4,7,1];
-// const newArray = array2.filter((element)=>{
-//    return element%2==0; 
+// promiseObject.then((res)=>{
+//     console.log(res);
+// }).catch((err)=>{
+//     console.log(err);
 // })
-//   console.log(array2);
-//   console.log(newArray);
+ 
+
+//promise.all return all data eksath when all data is resolve if one of the promise return reject the promise.all return reject ====>IMPORTANT
+// Promise.all([promiseObject,promiseObject2]).then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{console.log(err)});
 
 
-//reduce
-// const array3 = [3,2,4,1,7,8,7];
-// const answer = array3.reduce((sum,value)=>{
-//     return sum+value;
-// },0)
-// console.log(answer);
-    
 
-//slice 
-// 1) Not manipulate the original array
-// const array = ["promil", "tanu", "aali", "raj"];
-// const returnArray = array.slice(0,2);
-//   console.log(array);
-//   console.log(returnArray);
+// asyn/await
 
+// function getCheese()
+// {
+//     const readyCheese =  new Promise(function(resolve,reject){
+//         setTimeout(()=>{
+//             resolve("your cheese is ready");
+//         },3000)
+//     })
+//     // console.log(readyCheese);  //return promise with pending state
+//     console.log("woooo");
+//     return readyCheese;
+// }
 
-//splice
-// 1) manipulate the original array
-// 2)we can delete,replace,push element by splice method
-// const array = ["promil","ali","raj","amman"];
+// function makeDough(cheese)
+// {
+//     const readyDough = new Promise(function(resolve,reject){
+//         setTimeout(()=>{
+//             resolve("your dough is ready");
+//         },4000)
+//     })
+//     console.log("wooo");
+//     return readyDough;
+// }
 
-//delete:
-// array.splice(1,1);
-// console.log(array);
+// function bakePizza(dough)
+// {
+//     const readyPizza = new Promise(function(resolve,reject){
+//         setTimeout(()=>{
+//             resolve("your pizza is ready");
+//         },5000)
+//     })
+//     console.log("wooo")
+//     return readyPizza;
+// }
 
-//replace
-// array.splice(1,1,"tanu");
-// console.log(array);
+// async function getPromiseData(){
+//     const cheese = await getCheese();
+//     console.log(cheese)
+//     const dough = await makeDough(cheese);
+//     console.log(dough)
+//     const pizza = await bakePizza(dough);
+//     console.log(pizza)
+//     console.log("delivered....");
+// }
 
-// push
-// array.splice(1,0,"tanu");
-// console.log(array);
+// getPromiseData();
